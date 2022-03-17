9878a48c146d68d0d4aa16d23455c35cb34f23b6
Sorin Netejoru <netejoru.mihaisorin@yahoo.com>
2022-03-17 21:24:00 +0200

    HomeWok_03 - Updated

---
+++ HomeWork_03_Baze_de_Date/WebCarDealership/WebCarDealership/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using CarDealership.Data.Models;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using WebCarDealership.Requests;
+
+namespace WebCarDealership.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly DealershipDbContext _dbContext;
+
+        public CustomerController(DealershipDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        // De revenit !!!!
+        // La introducerea email-ului (format de email clasic) primesc o eroare 400
+        [HttpPost]
+        public async Task<IActionResult> Post(CustomerRequestModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var dbModel = new Customer
+            {
+                Name = model.Name,
+                Email = model.Email
+            };
+            _dbContext.Customers.Add(dbModel);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Created(Request.GetDisplayUrl(), dbModel);
+
+        }
+
+    }
+}
---
+++ HomeWork_03_Baze_de_Date/WebCarDealership/WebCarDealership/Requests/CustomerRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebCarDealership.Requests
+{
+    public class CustomerRequestModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
