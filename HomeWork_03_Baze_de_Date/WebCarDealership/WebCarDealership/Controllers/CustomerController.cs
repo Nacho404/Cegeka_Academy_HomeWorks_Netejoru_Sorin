527f0efdd9c9d71ea1d5302e66e2e10ece607a1e
Sorin Netejoru <netejoru.mihaisorin@yahoo.com>
2022-03-18 10:51:14 +0200

    HomeWork_03 - Updated - 18.03.22

--- HomeWork_03_Baze_de_Date/WebCarDealership/WebCarDealership/Controllers/CustomerController.cs
+++ HomeWork_03_Baze_de_Date/WebCarDealership/WebCarDealership/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using CarDealership.Data.Models;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebCarDealership.Requests;
 
 namespace WebCarDealership.Controllers
@@ -16,8 +17,17 @@ namespace WebCarDealership.Controllers
             _dbContext = dbContext;
         }
         
-        // De revenit !!!!
-        // La introducerea email-ului (format de email clasic) primesc o eroare 400
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var customers = await _dbContext.Customers.ToListAsync();
+            return Ok(customers);
+        }
+
+        
+
+
         [HttpPost]
         public async Task<IActionResult> Post(CustomerRequestModel model)
         {
@@ -37,6 +47,55 @@ namespace WebCarDealership.Controllers
             await _dbContext.SaveChangesAsync();
 
             return Created(Request.GetDisplayUrl(), dbModel);
+
+        }
+
+
+        [HttpPut("update-customer-by-id/{customerId}")]
+        public async Task<IActionResult> Put(int customerId, CustomerRequestModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customer = _dbContext.Customers.FirstOrDefault(x => x.Id == customerId);
+
+            if (customer != null)
+            {
+                customer.Name = model.Name;
+                customer.Email = model.Email;
+
+                await _dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                return NotFound("Customer ID not found in 'Customers' table");
+            }
+
+
+            return Ok(customer);
+        }
+
+
+
+        [HttpDelete("delete-customer-by-id/{customerId}")]
+        public async Task<IActionResult> Delete(int customerId)
+        {
+            var customer = _dbContext.Customers.FirstOrDefault(x => x.Id == customerId);
+
+            if(customer != null)
+            {
+                _dbContext.Customers.Remove(customer);
+                await _dbContext.SaveChangesAsync();
+            }else
+            {
+                return NotFound("Customer ID not found in 'Customers' table");
+            }
+
+            //return Delete(Request.GetDisplayUrl(), customer);
+
+            return Ok(customer);
 
         }
 
--- HomeWork_03_Baze_de_Date/WebCarDealership/WebCarDealership/Requests/CustomerRequestModel.cs
+++ HomeWork_03_Baze_de_Date/WebCarDealership/WebCarDealership/Requests/CustomerRequestModel.cs
@@ -1,9 +1,13 @@
-﻿namespace WebCarDealership.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCarDealership.Requests
 {
     public class CustomerRequestModel
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Email { get; set; }
     }
 }
