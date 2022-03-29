a350612cb582d8c84b6f14f2c2f2ee7de6c67ca5
Sorin Netejoru <netejoru.mihaisorin@yahoo.com>
2022-03-29 20:43:55 +0300

    'each' from 'RentalStore/foreach' changed in 'rental'

--- HomeWork_06_Clean_Code/RentalCars/RentalStore.cs
+++ HomeWork_06_Clean_Code/RentalCars/RentalStore.cs
@@ -30,41 +30,41 @@ namespace RentalCars
             var r = "Rental Record for " + _storeName + "\n";
             r += "------------------------------\n";
 
-            foreach (var each in _rentals)
+            foreach (var rental in _rentals)
             {
                 double thisAmount = 0;
 
                 // determines the amount for each line
-                switch (each.Car._priceCode)
+                switch (rental.Car._priceCode)
                 {
                     case PriceCode.Regular:
                         thisAmount += pricePerDay * 2;
-                        if (each._daysRented > 2)
-                            thisAmount += (each._daysRented - 2) * pricePerDay * 0.75;
+                        if (rental._daysRented > 2)
+                            thisAmount += (rental._daysRented - 2) * pricePerDay * 0.75;
                         break;
                     case PriceCode.Premium:
-                        thisAmount += each._daysRented * pricePerDay * 1.5;
+                        thisAmount += rental._daysRented * pricePerDay * 1.5;
                         break;
                     case PriceCode.Mini:
                         thisAmount += pricePerDay * 3 * 0.75;
-                        if (each._daysRented > 3)
-                            thisAmount += (each._daysRented - 3) * pricePerDay * 0.5;
+                        if (rental._daysRented > 3)
+                            thisAmount += (rental._daysRented - 3) * pricePerDay * 0.5;
                         break;
                 }
 
-                if (each.Customer.FrequentRenterPoints >= 5)
+                if (rental.Customer.FrequentRenterPoints >= 5)
                 {
                     thisAmount = thisAmount * 0.95;
                 }
 
                 frequentRenterPoints = 1;
-                if (each.Car._priceCode == PriceCode.Premium
-                    && each._daysRented > 1)
+                if (rental.Car._priceCode == PriceCode.Premium
+                    && rental._daysRented > 1)
                     frequentRenterPoints++;
 
-                each.Customer.FrequentRenterPoints += frequentRenterPoints;
+                rental.Customer.FrequentRenterPoints += frequentRenterPoints;
 
-                r += each.Customer._customerName + "\t" + each.Car._model + "\t" + each._daysRented + "d \t" + thisAmount + " EUR\n";
+                r += rental.Customer._customerName + "\t" + rental.Car._model + "\t" + rental._daysRented + "d \t" + thisAmount + " EUR\n";
                 totalAmount += thisAmount;
             }
             r += "------------------------------\n";
