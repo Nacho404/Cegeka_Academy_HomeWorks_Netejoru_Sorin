5a5275a33bfc90283d7296bb211801b0b9504eb9
Sorin Netejoru <netejoru.mihaisorin@yahoo.com>
2022-03-29 19:06:54 +0300

    I created a specific variable for each Car

--- HomeWork_06_Clean_Code/RentalCars/Program.cs
+++ HomeWork_06_Clean_Code/RentalCars/Program.cs
@@ -12,14 +12,23 @@ namespace RentalCars
             var mihaiChirica = new Customer("Mihai Chirica");
             var gigiBecali = new Customer("Gigi Becali");
 
-            iasiRentalsStore.AddRental(new Rental(ionPopescu, new Car(PriceCode.Regular, "Ford Focus"), 2));
-            iasiRentalsStore.AddRental(new Rental(gigiBecali, new Car(PriceCode.Regular, "Renault Clio"), 3));
-            iasiRentalsStore.AddRental(new Rental(ionPopescu, new Car(PriceCode.Premium, "BMW 330i"), 1));
-            iasiRentalsStore.AddRental(new Rental(gigiBecali, new Car(PriceCode.Premium, "Volvo XC90"), 3));
-            iasiRentalsStore.AddRental(new Rental(mihaiChirica, new Car(PriceCode.Mini, "Toyota Aygo"), 2));
-            iasiRentalsStore.AddRental(new Rental(ionPopescu, new Car(PriceCode.Mini, "Hyundai i10"), 4));
-            iasiRentalsStore.AddRental(new Rental(gigiBecali, new Car(PriceCode.Premium, "Volvo XC90"), 2));
-            iasiRentalsStore.AddRental(new Rental(gigiBecali, new Car(PriceCode.Premium, "Mercedes E320"), 1));
+            var fordFocus = new Car(PriceCode.Regular, "Ford Focus");
+            var renaultClio = new Car(PriceCode.Regular, "Renault Clio");
+            var bmw330i = new Car(PriceCode.Premium, "BMW 330i");
+            var volvoXC90 = new Car(PriceCode.Premium, "Volvo XC90");
+            var toyotaAygo = new Car(PriceCode.Mini, "Toyota Aygo");
+            var hyundaiI10 = new Car(PriceCode.Mini, "Hyundai i10");
+            var mercedesE320 = new Car(PriceCode.Premium, "Mercedes E320");
+
+
+            iasiRentalsStore.AddRental(new Rental(ionPopescu, fordFocus, 2));
+            iasiRentalsStore.AddRental(new Rental(gigiBecali, renaultClio, 3));
+            iasiRentalsStore.AddRental(new Rental(ionPopescu, bmw330i, 1));
+            iasiRentalsStore.AddRental(new Rental(gigiBecali, volvoXC90, 3));
+            iasiRentalsStore.AddRental(new Rental(mihaiChirica, toyotaAygo, 2));
+            iasiRentalsStore.AddRental(new Rental(ionPopescu, hyundaiI10, 4));
+            iasiRentalsStore.AddRental(new Rental(gigiBecali, volvoXC90, 2));
+            iasiRentalsStore.AddRental(new Rental(gigiBecali, mercedesE320, 1));
 
             Console.WriteLine(iasiRentalsStore.Statement());
             Console.ReadKey();
