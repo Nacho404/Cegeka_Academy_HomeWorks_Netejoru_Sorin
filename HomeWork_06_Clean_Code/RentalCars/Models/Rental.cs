66b28733e10757f2c389993aa78e4dd54bbb1dc6
Sorin Netejoru <netejoru.mihaisorin@yahoo.com>
2022-03-30 12:34:49 +0300

    I created 'CalculateRentalAmount' functionality inside the Rental class that calculate amount of each reantal

--- HomeWork_06_Clean_Code/RentalCars/Models/Rental.cs
+++ HomeWork_06_Clean_Code/RentalCars/Models/Rental.cs
@@ -5,12 +5,55 @@
         public Customer Customer;
         public Car Car;
         public int _daysRented;
+        public double rentalAmount;
+
 
         public Rental(Customer customer, Car car, int daysRented)
         {
             Customer = customer;
             Car = car;
             _daysRented = daysRented;
+            rentalAmount = CalculateRentalAmount();
+        }
+
+        private double CalculateRentalAmount()
+        {
+            double pricePerDay = 20;// why is the price 20 euro for all car categories?
+            double amount = 0;
+
+            // determines the amount for each line
+            switch (Car._priceCode)
+            {
+                case PriceCode.Regular:
+                    amount += pricePerDay * 2;
+                    if (_daysRented > 2)
+                        amount += (_daysRented - 2) * pricePerDay * 0.75;
+                    break;
+                case PriceCode.Premium:
+                    amount += _daysRented * pricePerDay * 1.5;
+                    break;
+                case PriceCode.Mini:
+                    amount += pricePerDay * 3 * 0.75; // here is the bug
+                    if (_daysRented > 3)
+                        amount += (_daysRented - 3) * pricePerDay * 0.5;
+                    break;
+            }
+
+            if (Customer._frequentRenterPoints >= 5)
+            {
+                amount = amount * 0.95;
+            }
+
+
+            if (Car._priceCode == PriceCode.Premium
+                    && _daysRented > 1)
+            {
+                Customer._frequentRenterPoints++;
+            }
+
+            Customer._frequentRenterPoints++;
+
+            return amount;
         }
     }
 }
--- HomeWork_06_Clean_Code/RentalCars/Store.cs
+++ HomeWork_06_Clean_Code/RentalCars/Store.cs
@@ -22,50 +22,15 @@ namespace RentalCars
 
         public string Statement()
         {
-            double pricePerDay = 20;// why is the price 20 euro for all car categories?
             double totalAmount = 0;
-            var frequentRenterPoints = 0;// i dont need this variable
 
             var r = "Rental Record for " + _storeName + "\n";
             r += "------------------------------\n";
 
             foreach (var rental in Rentals)
             {
-                double thisAmount = 0;// i must change the name of this variable
-
-                // determines the amount for each line
-                switch (rental.Car._priceCode)
-                {
-                    case PriceCode.Regular:
-                        thisAmount += pricePerDay * 2;
-                        if (rental._daysRented > 2)
-                            thisAmount += (rental._daysRented - 2) * pricePerDay * 0.75;
-                        break;
-                    case PriceCode.Premium:
-                        thisAmount += rental._daysRented * pricePerDay * 1.5;
-                        break;
-                    case PriceCode.Mini:
-                        thisAmount += pricePerDay * 3 * 0.75; // here is the bug
-                        if (rental._daysRented > 3)
-                            thisAmount += (rental._daysRented - 3) * pricePerDay * 0.5;
-                        break;
-                }
-
-                if (rental.Customer._frequentRenterPoints >= 5)
-                {
-                    thisAmount = thisAmount * 0.95;
-                }
-
-                if (rental.Car._priceCode == PriceCode.Premium
-                    && rental._daysRented > 1)
-                {
-                    rental.Customer._frequentRenterPoints++;
-                }
-
-                rental.Customer._frequentRenterPoints ++;
-
-                r += rental.Customer._customerName + "\t" + rental.Car._model + "\t" + rental._daysRented + "d \t" + thisAmount + " EUR\n";
-                totalAmount += thisAmount;
+                r += rental.Customer._customerName + "\t" + rental.Car._model + "\t" + rental._daysRented + "d \t" + rental.rentalAmount + " EUR\n";
+                totalAmount += rental.rentalAmount;
             }
             r += "------------------------------\n";
             r += "Total revenue " + totalAmount + " EUR\n";
